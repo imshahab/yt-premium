f3fee307ed8fe3d0be8dfdfad74574b2eee26475
Shahab Hosseini <63445114+theshahabhosseini@users.noreply.github.com>
2023-09-15 12:58:57 +0330

    Add files via upload

---
+++ background.js
@@ -0,0 +1,14 @@
+// const checkBoxEl = document.getElementById("switch");
+// !localStorage.getItem("premiumEnabled") &&
+//   localStorage.setItem("premiumEnabled", true);
+
+// let enabled = localStorage.getItem("premiumEnabled") === "true" ? true : false;
+
+//checkBoxEl.checked =
+//localStorage.getItem("premiumEnabled") === "true" ? true : false;
+
+// checkBoxEl.addEventListener("change", () => {
+//   localStorage.setItem("premiumEnabled", checkBoxEl.checked);
+// });
+
+
